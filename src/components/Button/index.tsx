d084b6c15f5da3c9c869ad374877a9c2a3049fe9
AlysonDEV <devalyson@gmail.com>
2022-04-16 11:43:06 -0300

    Criado o estado dentro do componente

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 type ButtonProps = {
   children?:string;
 }
 
 export function Button (props: ButtonProps) {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+  
   return (
-    <button>{props.children || "default"}</button>
+    <button onClick={increment}>{props.children || "default"} - {count}</button>
   )
 }
